0ae4be7d196d3ed29a7063d602179ef116ef9445
Ciro Costa <ciro.fga@gmail.com>
2023-01-01 22:26:56 -0300

    fix(): Added server port on description to app.listen

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -17,4 +17,4 @@ route.get('/', (req: Request, res: Response) => {
 
 app.use(route)
 
-app.listen(process.env.PORT ?? 3000, () => 'server running on port 3001')
+app.listen(process.env.PORT ?? 3000, () => `server running on port ${process.env.PORT ?? 3000}`)
