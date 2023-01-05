114487747b9663f634ddc797e7bc259de185e31f
Ciro Costa <ciro.fga@gmail.com>
2023-01-05 16:31:32 -0300

    hotfix(login.dto.ts): Removed max size password

--- back-end/src/Dto/login.dto.ts
+++ back-end/src/Dto/login.dto.ts
@@ -4,7 +4,7 @@ import { Prisma } from '@prisma/client';
 
 export const LoginDto = object({
     email: refine(string(), 'email', (v) => isEmail.validate(v)),
-    password: size(string(), 8, 40)
+    password: size(string(), 8)
 });
 
 export type LoginDtoType = Omit<Prisma.UserCreateArgs['data'], 'id' | 'nameComplete' | 'birhtDate' | 'createdDate' | 'active'>
