cbbccbf753eeb7adea301184a0e592bb2ae1478c
Paulo H <ph.hr.001@gmail.com>
2023-01-05 21:21:26 -0300

    feat(atoms): modify text component style

--- front-admin/src/components/atoms/Text.tsx
+++ front-admin/src/components/atoms/Text.tsx
@@ -3,12 +3,15 @@ import styled from "styled-components";
 interface TextInterface {
   color?: string;
   size?: string;
+  margin?: string;
+  fontWeight?: string;
 }
 
 const Text = styled.span<TextInterface>`
-  font-size: ${({size}) => size || '18px'};
-  font-weight: bold;
-  color: ${({color}) => color}
+  font-size: ${({ size }) => size || '18px'};
+  font-weight:${({ fontWeight }) => fontWeight || 'bold'};
+  margin: ${({ margin }) => margin};
+  color: ${({ color }) => color}
 `
 
 export default Text;
