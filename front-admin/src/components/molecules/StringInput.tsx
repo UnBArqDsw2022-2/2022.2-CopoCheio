28731b29fffd5601c7e882ecb65e1d2ebcf1ce90
João Victor Batista <jvtexbat@hotmail.com>
2023-01-27 09:27:28 -0300

    feat(front-admin): update string input component

--- front-admin/src/components/molecules/StringInput.tsx
+++ front-admin/src/components/molecules/StringInput.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import Icon from '../atoms/Icon/Icon';
+import { IconsTypes } from '../atoms/Icon/IconTypes';
 import Text from '../atoms/Text';
 
 interface StringInputInterface {
@@ -11,31 +13,53 @@ interface StringInputInterface {
     onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
     value?: string;
     inputError?: string
+    icon?: IconsTypes
 }
 
 interface GenericStringInputInterface extends Omit<StringInputInterface, "placeholder"> {
     type: "text" | "email" | "password",
 }
 
+interface GenericInputFieldInterface {
+    borderRadius?: string,
+}
+
+interface GenericInputContainerInterface {
+    width?: string;
+}
+
 const GenericStringInput = styled.input<GenericStringInputInterface>`
-    width: ${({ width }) => width || '100%'};
+    flex-grow: 1;
     height: ${({ height }) => height || '40px'};
-    border-radius: ${({ borderRadius }) => borderRadius || '4px'};
-    border: 1px solid #ced4da;
-    padding: 0.375rem 0.75rem;
     font-size: 1rem;
     line-height: 1.5;
-    color: #495057;
+    border: none;
+    padding: 0px;
+`
+
+const GenericInputField = styled.div<GenericInputFieldInterface>`
+    flex-grow: 1;
+    width: 100%;
+    max-height: 60px;
+    border: 1px solid #ced4da;
+    border-radius: ${({ borderRadius }) => borderRadius || '8px'};
+    padding: 1rem;
+    cursor: pointer;
     background-color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #495057;
     background-clip: padding-box;
     transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 `
 
-const GenericInputField = styled.div`
+const GenericInputContainer = styled.div<GenericInputContainerInterface>`
     display: flex;
     flex-direction: column;
-    width: 100%;
     align-items: flex-start;
+    width: ${({ width }) => width || '100%'};
+    gap: 0.5rem;
 `
 
 const StringInput = ({
@@ -48,22 +72,31 @@ const StringInput = ({
     value,
     fontSize,
     inputError,
+    icon
 }: StringInputInterface) => {
     return (
-        <GenericInputField>
+        <GenericInputContainer width={width}>
             {placeholder && <Text color='grey' size='1em'>{placeholder}</Text>}
-            <GenericStringInput
-                data-testid='email-input'
-                type={type || 'text'}
-                width={width}
-                height={height}
-                borderRadius={borderRadius}
-                onChange={onChange}
-                value={value}
-                fontSize={fontSize}
-            />
+            <GenericInputField borderRadius={borderRadius}>
+                {icon &&
+                    <Icon
+                        icon={icon}
+                        size={'24px'}
+                        marginRight={'8px'}
+                    />
+                }
+
+                <GenericStringInput
+                    data-testid='email-input'
+                    type={type || 'text'}
+                    height={height}
+                    onChange={onChange}
+                    value={value}
+                    fontSize={fontSize}
+                />
+            </GenericInputField >
             {inputError && <Text color='red' size='14px' margin='4px 0px'>{inputError}</Text>}
-        </GenericInputField >
+        </GenericInputContainer>
     )
 }
 
