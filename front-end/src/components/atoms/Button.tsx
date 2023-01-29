1d0510d759582d3f48ec2588c3c3afe801d161fc
João Victor Batista <jvtexbat@hotmail.com>
2023-01-29 19:20:37 -0300

    feat(front-end): create button component

---
+++ front-end/src/components/atoms/Button.tsx
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components';
+
+interface ButtonInterface {
+    variant?: 'primary' | 'secondary',
+    children: string,
+    onClick: VoidFunction
+}
+
+interface GenericButtonInterface extends Omit<ButtonInterface, "onClick" | "children"> { }
+
+const GenericButton = styled.button<GenericButtonInterface>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+    padding: 16px;
+    border-radius: 999px;
+    border: none;
+    cursor: pointer;
+    font-family: 'Work Sans', sans-serif;
+    font-weight: 600;
+
+    ${({ variant }) => {
+        switch (variant) {
+            case 'primary':
+                return css`
+                    background-color: ${({ theme }) => theme.secondary};
+                    color: ${({ theme }) => theme.primary};
+                `
+            case 'secondary':
+                return css`
+                    background-color: transparent;
+                    border: ${({ theme }) => `2px solid ${theme.secondary}`};
+                    color: ${({ theme }) => theme.secondary};
+                `
+        }
+    }}
+`
+
+const Button = ({ variant, children, onClick }: ButtonInterface) => {
+    return (
+        <GenericButton
+            data-testid='main button'
+            onClick={onClick}
+            variant={variant || 'primary'}
+        >
+            {children.toUpperCase()}
+        </GenericButton>
+    )
+}
+
+export default Button;
--- front-end/src/styles/styled.d.ts
+++ front-end/src/styles/styled.d.ts
@@ -3,14 +3,9 @@ import 'styled-components';
 declare module 'styled-components' {
     export interface DefaultTheme {
         primary: string,
-        alternative: string,
-        success: string,
-        denied: string,
-        black: string,
-        loading: string,
+        alternative_primary: string,
+        secondary: string,
         white: string,
-        alternative_white: string,
-        grey: string
         atoms: {
             title: string,
             text: string,
