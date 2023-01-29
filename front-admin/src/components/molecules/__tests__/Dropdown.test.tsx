89125e7fe709a601a6125c6851743fc94ae89d49
João Victor Batista <jvtexbat@hotmail.com>
2023-01-29 18:19:12 -0300

    feat(front-admin): update tests

--- front-admin/src/components/molecules/__tests__/Dropdown.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { Dropdown } from "../Dropdown";
-
-describe("#Dropdown", () => {
-    test("initial test expected to fail", () => {
-
-        expect(1+2).toBe(4);
-    });
-});
