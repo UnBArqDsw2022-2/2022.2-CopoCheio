3b87f97cfd4731cec7d52aa2ba570d2bac9c2810
fmaachadoo <filipe__machado@hotmail.com>
2023-01-29 10:59:03 -0300

    feat(front-admin/ListUsers): add modal

--- front-admin/src/components/templates/ListUsersTemplate.tsx
+++ front-admin/src/components/templates/ListUsersTemplate.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import styled from "styled-components";
 import User from "../../models/UserModel";
 import Card from "../organisms/Card";
+import Text from "../atoms/Text";
+import {createPortal} from "react-dom";
 
 const ListUserTemplateStyle = styled.div`
     display: flex;
@@ -12,11 +14,42 @@ const user = new User('Paulo', 'ph.hr.001@gmail.com');
 
 const listUserByRequest = [user, user, user, user];
 
+
+const ModalWrapper = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// Create function called showActionModal that receives a User and a string to open a modal
+
+
 const ListUserTemplate = () => {
+
+    const ActionModal = (userId: string, action: string) => {
+        console.log(action);
+        return (
+            <ModalWrapper>
+                <Text>Modal Content</Text>
+                <Text>{userId}</Text>
+            </ModalWrapper>
+        );
+    };
+
+    const [showModal, setShowModal] = useState(false);
+    const [modalContent, setModalContent] = useState<React.ReactElement | null>(null);
+
     useEffect(() => { }, []);
     return (
         <ListUserTemplateStyle>
-            {listUserByRequest.map(function (user: User) {
+            {
+                listUserByRequest.map(function (user: User) {
                 return (
                     <>
                         <Card
@@ -24,12 +57,17 @@ const ListUserTemplate = () => {
                             cardType="user"
                             height="291px"
                             width="227px"
+                            onBlockUser={ () => {
+                                setModalContent(ActionModal(user.email!, 'block'));
+                                setShowModal(true);
+                            }}
                         />
                         <div style={{ width: "56px" }}></div>
                     </>
                 );
             })
-            }
+        }
+        {showModal && createPortal(modalContent, document.body)}
         </ListUserTemplateStyle>
     )
 }
