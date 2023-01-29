2847ebe503b1112647f7f8229ebfa76a0a6ea7b4
João Victor Batista <jvtexbat@hotmail.com>
2023-01-29 18:38:57 -0300

    feat(front-end): create style pallete

---
+++ front-end/src/styles/colors.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export const pallete = {
+  primary: '#191C24',
+  alternative_primary: '#272F38',
+  secondary: '#DAB783',
+  white: '#fff'
+};
+
+export const colors = {
+  ...pallete,
+  atoms: {
+    title: pallete.secondary,
+    text: pallete.secondary,
+    subtitle: pallete.secondary,
+  }
+}
